e8b21957672085741635d6298f1e0bd216eb80ca
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-10-12 12:16:38 -0400

    Created Main Classes for Client/Server

---
+++ src/HockeyLive/Server/Server.java
@@ -0,0 +1,10 @@
+package HockeyLive.Server;
+
+/**
+ * Created by Michaël on 10/12/2015.
+ */
+public class Server {
+    public static void main(String[] args){
+        System.out.println("Hello World.");
+    }
+}
