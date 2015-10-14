c086bc1a6b5db1e9680da088d6bcb83d0575bab1
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-10-14 15:17:36 -0400

    Created Game Factory server-side. Corrected issues in common models.

--- src/HockeyLive/Server/Factory/GameFactory.java
+++ src/HockeyLive/Server/Factory/GameFactory.java
@@ -1,13 +1,32 @@
 package HockeyLive.Server.Factory;
 
+import HockeyLive.Common.Models.Game;
+import HockeyLive.Common.Models.GameInfo;
+
 /**
  * Created by Michaël on 10/14/2015.
  */
 public class GameFactory{
-    private static int GAME_ID = 0;
+    private static int gameId = 0;
+    private static int periodLength = 20;  // in minutes
 
-    private static int GenerateID()
+    private static int GetNextID()
     {
-        return ++GAME_ID;
+        return ++gameId;
+    }
+
+    private static Game GenerateGame(String host, String visitor){
+        Game newGame = new Game(GetNextID(),host,visitor);
+        return newGame;
+    }
+
+    private static GameInfo GenerateGameInfo(Game game){
+        GameInfo info = new GameInfo(game.getGameID(), periodLength);
+
+        return info;
+    }
+
+    public static void UpdatePeriodLength(int minutes){
+        periodLength = minutes;
     }
 }
