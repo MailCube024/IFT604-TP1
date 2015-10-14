3a1861d5c8fb69830df3f393aced4edc8f44df11
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-10-14 17:08:54 -0400

    Added classes for chronometers.

--- src/HockeyLive/Server/Server.java
+++ src/HockeyLive/Server/Server.java
@@ -19,11 +19,12 @@ import java.util.concurrent.Executors;
 /**
  * Created by Michaël on 10/12/2015.
  */
-public class Server {
+public class Server implements Runnable {
     private List<Game> runningGames;
     private ConcurrentMap<Game,GameInfo> runningGameInfos;
     private ConcurrentMap<Game,List<Bet>> placedBets;
     private ServerSocket socket;
+    private Thread serverThread;
 
     public Server() {
         runningGames = new ArrayList<>();
@@ -52,11 +53,6 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args){
-        Server server = new Server();
-        server.execute();
     }
 
     public void SendReply(ClientMessage clientMessage, Object data) {
@@ -109,5 +105,20 @@ public class Server {
 
     public void SendResults(Game game) {
 
+    }
+
+    @Override
+    public void run() {
+        execute();
+    }
+
+    public void start(){
+        serverThread = new Thread(this);
+        serverThread.start();
+    }
+
+    public static void main(String[] args){
+        Server server = new Server();
+        server.start();
     }
 }
