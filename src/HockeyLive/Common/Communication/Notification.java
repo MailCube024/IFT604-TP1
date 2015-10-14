d6b82d598aa20d5733c4ef6143bbaccda3f8b968
Michael Beaulieu <michael.beaulieu@hotmail.com>
2015-10-14 12:22:20 -0400

    Added getters and constructor

--- src/HockeyLive/Common/Communication/Notification.java
+++ src/HockeyLive/Common/Communication/Notification.java
@@ -12,4 +12,38 @@ public class Notification implements Serializable {
     private InetAddress receiverIp;
     private int receiverPort;
     private int requestID;
+    private Object data;
+
+    public Notification(InetAddress senderIp, int senderPort, InetAddress receiverIp, int receiverPort, int requestID, Object data) {
+        this.senderIp = senderIp;
+        this.senderPort = senderPort;
+        this.receiverIp = receiverIp;
+        this.receiverPort = receiverPort;
+        this.requestID = requestID;
+        this.data = data;
+    }
+
+    public InetAddress getSenderIp() {
+        return senderIp;
+    }
+
+    public int getSenderPort() {
+        return senderPort;
+    }
+
+    public InetAddress getReceiverIp() {
+        return receiverIp;
+    }
+
+    public int getReceiverPort() {
+        return receiverPort;
+    }
+
+    public int getRequestID() {
+        return requestID;
+    }
+
+    public Object getData() {
+        return data;
+    }
 }
